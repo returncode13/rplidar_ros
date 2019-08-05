84942f4bae27dbfa53b0dee9897cb25bcefc9d95
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2019-08-05 15:39:50 +0900

    succeed building

---
+++ include/rplidar_ros/rplidar_node.hpp
@@ -0,0 +1,78 @@
+/*
+ *  RPLIDAR ROS NODE
+ *
+ *  Copyright (c) 2009 - 2014 RoboPeak Team
+ *  http://www.robopeak.com
+ *  Copyright (c) 2014 - 2016 Shanghai Slamtec Co., Ltd.
+ *  http://www.slamtec.com
+ *
+ */
+/*
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef RPLIDAR_ROS__RPLIDAR_ROS_HPP_
+#define RPLIDAR_ROS__RPLIDAR_ROS_HPP_
+
+#include <memory>
+#include <string>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/laser_scan.hpp>
+#include <std_srvs/srv/empty.hpp>
+#include "rplidar.h"
+
+namespace rplidar_ros
+{
+
+class RPlidarNode : public rclcpp::Node
+{
+public:
+  RPlidarNode(const std::string & name = "rplidar_node",
+              const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
+
+  void publish_scan();
+
+  bool get_device_info();
+
+  bool check_health();
+
+  bool stop_motor(const std::shared_ptr<rmw_request_id_t> request_header,
+                  std_srvs::srv::Empty::Request::SharedPtr request,
+                  std_srvs::srv::Empty::Response::SharedPtr response);
+
+  bool start_motor(const std::shared_ptr<rmw_request_id_t> request_header,
+                   std_srvs::srv::Empty::Request::SharedPtr request,
+                   std_srvs::srv::Empty::Response::SharedPtr response);
+
+  float get_angle(const rplidar_response_measurement_node_hq_t& node);
+
+  void spin();
+
+private:
+  std::unique_ptr<rp::standalone::rplidar::RPlidarDriver> drv;
+};
+
+}  // namespace rplidar_ros
+
+#endif  // RPLIDAR_ROS__RPLIDAR_ROS_HPP_
